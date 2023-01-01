afb92216d525e8e5a1ebd19a4a92b3b55836311d
Omwitsa <omwitsawilson@yahoo.com>
2023-01-02 02:24:55 +0300

    Save changes

--- Check.php
+++ Check.php
@@ -1,2 +1,2 @@
-docker build -t newtech_website .
-docker run -dit --name newtech-website -p 80:80 omwitsa/newtech_website
+docker build -t omwitsa/newtech_website .
+docker run --pull=always -dit --name newtech-website -p 80:80 omwitsa/newtech_website
